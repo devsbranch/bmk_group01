a6e30d4972d1ca308aa6d98af21dbf2ed5152129
Patricia-426 <73366296+Patricia-426@users.noreply.github.com>
2020-11-14 04:46:55 -0800

    added dockstrings snd coments

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 import database
 
 
-def user_menu():  # Presents the user with options available for selection
+def user_menu():
+    """This presents the user with options available for selection.
+    :argument None
+    :parameter None
+    """
     user_options = {
         'A': 'Create new bookmark',
         'R': 'Read bookmark (s)',
@@ -13,20 +17,26 @@ def user_menu():  # Presents the user with options available for selection
         print(f'{action} : {descr}')
 
 
-def user_input():  # Will process user inputs entered bu user from command line
+def user_input():
+    """This will process user inputs entered by the user from command line.
+    we have one parameter in this function that is the user_choice.
+    """
     while True:  # The while loop will keep the program running and the user is the one stop the program by entering
         # 'Q' (quit).
-        user_menu()  # Will show available options
+        user_menu()
         user_choice = input('Please select a letter option from the menu above: ')
 
         if user_choice not in ['A', 'R', 'U', 'D']:
             print("INVALID OPTION SELECTED")
             continue
-            # If user enters option other than which is available, user will e shown "INVALID OPTION SELECTED".
             # The continue statement makes it possible to go back to the beginning of the loop and prompt user to enter
             # the correct option
 
         elif user_choice == 'A':
+            """Within this function there are three more parameters,
+            that is bmk_name, bmk_url,bmk_desc that will allow the user to enter their input.
+            """
+
             bmk_name = input("Enter a name for your Bookmark:  ")
             bmk_url = input("Enter url for your Bookmark:  ")
             bmk_desc = input("Enter description for your Bookmark:  ")
@@ -36,6 +46,10 @@ def user_input():  # Will process user inputs entered bu user from command line
             continue
 
         elif user_choice == 'R':
+            """If the user selects R from the menu this should just show details that were entered.
+            where the for loop on index 0, shows the index where bmk_name is and where 1 shows bmk_url
+            and where 2 shows the description.
+            """
             bookmarks = database.show_bookmarks()
             for i in bookmarks:
                 print(i[0], i[1], i[2])
@@ -43,6 +57,10 @@ def user_input():  # Will process user inputs entered bu user from command line
             continue
 
         elif user_choice == 'U':
+            """within this parameter we have two more parameters.
+            That is bmk_to_update where the input the user enters will  be stored in that variable
+            the same happens with values_to_update.
+            """
             bmk_to_updt = input('What is the name of your bookmark you want to update?   ')
             values_to_updt = input(f"""What do you want to update to {bmk_to_updt}? Select option
                                 1. Everything
@@ -80,6 +98,8 @@ def user_input():  # Will process user inputs entered bu user from command line
             database.save_changes()
             continue
         elif user_choice == 'D':
+            """This parameter allows the user to delete the bookmark they no longer want."""
+
             bmk_name = input("Enter a name for your Bookmark you want to delete:  ")
             database.delete_bookmark(bmk_name)
             database.save_changes()
@@ -88,4 +108,4 @@ def user_input():  # Will process user inputs entered bu user from command line
 
 
 user_input()
-
+"""This function just storing the input entered by the user."""
