d053feccbcb81d3f3e085bd71fe4112bfbb52946
Jachin-Manda <manda.jachin482@gmail.com>
2020-11-16 22:38:36 +0200

    refactor code to use classes

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
-import database
+from database import CreateBookmark, ReadBookmarks, UpdateBookmark, DeleteBookmark, save_changes
 from datetime import datetime
 from datetime import date
 
 now = datetime.now()
-time_updated = now.strftime("%H:%M:%S")
-date_updated = date.today()
+current_time = now.strftime("%H:%M:%S")
+current_date = date.today()
 
 
 def user_menu():
@@ -30,6 +30,9 @@ def user_input():
     """
     while True:  # The while loop will keep the program running and the user is the one stop the program by entering
         # 'Q' (quit).
+        save_changes()  # the function will run when the while loop is exited since the user selects option Q.
+        # then function will close the connection to the database.
+        
         user_menu()
         user_choice = input('Please select a letter option from the menu above: ')
 
@@ -41,8 +44,8 @@ def user_input():
             bmk_name = input("Enter a name for your Bookmark:  ")
             bmk_url = input("Enter url for your Bookmark:  ")
             bmk_desc = input("Enter description for your Bookmark:  ")
-            database.add_bookmark(bmk_name, bmk_url, bmk_desc)
-            database.save_changes()
+            create_bmk = CreateBookmark(bmk_name, bmk_url, bmk_desc, current_date, current_time)
+            create_bmk.add_bookmark()
             print("bookmark added")
             continue
 
@@ -51,11 +54,9 @@ def user_input():
             where the for loop on index 0, shows the index where bmk_name is and where 1 shows bmk_url
             and where 2 shows the description.
             """
-            bookmarks = database.show_bookmarks()
-            for i in bookmarks:
-                print(
-                    f"bookmark name > {i[0]},  bookmark url > {i[1]},  bookmark description > {i[2]}, date added > {i[3]},  time > {i[4]}")
-            print("Process finished")
+            bookmarks = ReadBookmarks()
+            for i in bookmarks.show_bookmarks():
+                print(f"bookmark name > {i[0]},  bookmark url > {i[1]},  bookmark description > {i[2]}, date added > {i[3]},  time > {i[4]}") 
             continue
 
         elif user_choice == 'U':
@@ -65,49 +66,54 @@ def user_input():
             """
             bmk_to_updt = input('What is the name of your bookmark you want to update?   ')
             values_to_updt = input(f"""What do you want to update to {bmk_to_updt}? Select option
-                                1. Everything
+ q                              1. Everything
                                 2. change the bookmark name
                                 3. change the bookmark url
                                 4. change the bookmark description
                                 """)
-            while True:  # the loop will keep running the code inside until the user selects a valid option
-                # then the loop will break
+
+            updt_bmk = UpdateBookmark(current_date, current_time)
+
+            # the loop will keep running the code inside until the user selects a valid option
+            # then the loop will break
+            while True:
 
                 if values_to_updt == '1':
                     bmk_name = input("Enter a name for your Bookmark:  ")
                     bmk_url = input("Enter url for your Bookmark:  ")
                     bmk_desc = input("Enter description for your Bookmark")
-                    database.update_bookmark('1', bmk_to_updt, bmk_name, bmk_url, bmk_desc, date_updated, time_updated)
+                    updt_bmk.update_bookmark('1', bmk_to_updt, bmk_name, bmk_url, bmk_desc, current_date, current_time)
                     print('bookmark updated')
                     break
+
                 elif values_to_updt == '2':
                     bmk_name = input("Enter a name for your Bookmark:  ")
-                    database.update_bookmark('2', bmk_to_updt, bmk_name, '', '', date_updated, time_updated)
+                    updt_bmk.update_bookmark('2', bmk_to_updt, bmk_name, current_date, current_time)
                     print('bookmark updated')
                     break
+
                 elif values_to_updt == '3':
                     bmk_url = input("Enter a url for your Bookmark:  ")
-                    database.update_bookmark('3', bmk_to_updt, '', bmk_url, '', date_updated, time_updated)
+                    updt_bmk.update_bookmark('3', bmk_to_updt, bmk_url, current_date, current_time)
                     print('bookmark updated')
                     break
 
                 elif values_to_updt == '4':
                     bmk_desc = input("Enter a description for your Bookmark:  ")
-                    database.update_bookmark('4', bmk_to_updt, '', '', bmk_desc, date_updated, time_updated)
+                    updt_bmk.update_bookmark('4', bmk_to_updt, bmk_desc, current_date, current_time)
                     print('bookmark updated')
                     break
                 elif values_to_updt not in ['1', '2', '3', '4']:
                     print("invalid option")
                     break
-            database.save_changes()
             continue
 
         elif user_choice == 'D':
             """This parameter allows the user to delete the bookmark they no longer want."""
 
             bmk_name = input("Enter a name for your Bookmark you want to delete:  ")
-            database.delete_bookmark(bmk_name)
-            database.save_changes()
+            del_bmk = DeleteBookmark(bmk_name)
+            del_bmk.delete_bookmark()
             print("Bookmark deleted")
             continue
 
@@ -118,8 +124,6 @@ def user_input():
         else:
             print("INVALID OPTION SELECTED")
             continue
-    database.db_conn_close()  # the function will run when the while loop is exited since the user selects option Q.
-    # then function will close the connection to the database.
 
 
 user_input()
--- database.py
+++ database.py
@@ -2,29 +2,35 @@ import sqlite3
 from datetime import datetime
 from datetime import date
 
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-current_date = date.today()
+# now = datetime.now()
+# current_time = now.strftime("%H:%M:%S")
+# current_date = date.today()
 
 
-conn = sqlite3.connect('mydb.db')
+conn = sqlite3.connect('mydb.db')  # connect to database
 c = conn.cursor()
 
-
 c.execute("""
-             CREATE TABLE IF NOT EXISTS bookmarks 
-             (
-             bookmark_name TEXT NOT NULL, 
-             bookmark_url TEXT NOT NULL, 
-             bookmark_description TEXT NOT NULL, 
-             date_added TEXT NOT NULL, 
-             time TEXT NOT NULL)
-          """)
+            CREATE TABLE IF NOT EXISTS bookmarks 
+            (
+            bookmark_name TEXT NOT NULL, 
+            bookmark_url TEXT NOT NULL, 
+            bookmark_description TEXT NOT NULL, 
+            date_added TEXT NOT NULL, 
+            time TEXT NOT NULL)
+        """)
 
 
-def add_bookmark(bmk_name, bmk_url, bmk_desc):
-    c.execute(
-        f"""
+class CreateBookmark:  # This class create a bookmark using the add_bookmark method. The properties will be the values the bookmark will have.
+    def __init__(self, bmk_name, bmk_url, bmk_desc, date, time):
+        self.bmk_name = bmk_name
+        self.bmk_url = bmk_url
+        self.bmk_desc = bmk_desc
+        self.date = date
+        self.time = time
+
+    def add_bookmark(self): # This method is called when user selects option 'C' to create a bookmark with values entered by user. 
+        c.execute(f"""
             INSERT INTO bookmarks (
             bookmark_name, 
             bookmark_url, 
@@ -32,55 +38,65 @@ def add_bookmark(bmk_name, bmk_url, bmk_desc):
             date_added, 
             time
             ) 
-            VALUES ('{bmk_name}', '{bmk_url}', '{bmk_desc}', '{current_date}', '{current_time}')""")
+            VALUES ('{self.bmk_name}', '{self.bmk_url}', '{self.bmk_desc}', '{self.date}', '{self.time}')""")
 
 
-def show_bookmarks():
-    return c.execute("""SELECT * FROM bookmarks""")  # the function runs only if the user selects option R
+class ReadBookmarks(object): # This class is will initiate when user selects option 
+    def show_bookmarks(self):
+        return c.execute("""SELECT * FROM bookmarks""")  # the function runs only if the user selects option R
 
 
-def update_bookmark(opt, bmk_to_updt, bmk_name, bmk_url,
-                    bmk_desc, date_uptd, time):
-    if opt == '1':
-        c.execute(
-            f"""
-                UPDATE bookmarks SET 
-                bookmark_name = '{bmk_name}', bookmark_url = '{bmk_url}', 
-                bookmark_description = '{bmk_desc}', 
-                date_added = '{date_uptd}', 
-                time = '{time}'
-                WHERE bookmark_name = '{bmk_to_updt}'
-            """)
+class UpdateBookmark:
+    def __init__(self, date, time):
+        self.date = date
+        self.time = time
+        
+    def update_bookmark(self, *args): # the update_bookmark method will be called whenuser selects option 'U'. The *args parameter is used
+                                      # since the number of arguments is not fixed and will vary according to what the user wants to update to the bookmark
+                                      # So the arguments will passed into the sql query by getting the positionof the argument required using indexes.
+        if args[0] == '1':
+            c.execute(
+                f"""
+                    UPDATE bookmarks SET 
+                    bookmark_name = '{args[2]}', bookmark_url = '{args[3]}', 
+                    bookmark_description = '{args[4]}', 
+                    date_added = '{self.date}', 
+                    time = '{self.time}'
+                    WHERE bookmark_name = '{args[1]}'
+                """)
 
-    elif opt == '2':
-        c.execute(f"""
-                      UPDATE bookmarks SET bookmark_name = '{bmk_name}' 
-                      date_added = '{date_uptd}', 
-                      time = '{time}'
-                      WHERE bookmark_name = '{bmk_to_updt}'
-                   """)
+        elif args[0] == '2':
+            c.execute(f"""
+                            UPDATE bookmarks SET bookmark_name = '{args[2]}',
+                            date_added = '{self.date}', 
+                            time = '{self.time}'
+                            WHERE bookmark_name = '{args[1]}'
+                        """)
 
-    elif opt == '3':
-        c.execute(f"""
-                      UPDATE bookmarks SET bookmark_url = '{bmk_url}' 
-                      date_added = '{date_uptd}', 
-                      time = '{time}'
-                      WHERE bookmark_name = '{bmk_to_updt}'""")
+        elif args[0] == '3':
+            c.execute(f"""
+                            UPDATE bookmarks SET bookmark_url = '{args[2]}',
+                            date_added = '{self.date}', 
+                            time = '{self.time}'
+                            WHERE bookmark_name = '{args[1]}'""")
 
-    elif opt == '4':
-        c.execute(f"""
-                      UPDATE bookmarks SET bookmark_description = '{bmk_desc}',
-                      date_added = '{date_uptd}', 
-                      time = '{time}'
-                      WHERE bookmark_name = '{bmk_to_updt}'
-                   """)
+        elif args[0] == '4':
+            c.execute(f"""
+                            UPDATE bookmarks SET bookmark_description = '{args[2]}',
+                            date_added = '{self.date}', 
+                            time = '{self.time}'
+                            WHERE bookmark_name = '{args[1]}'
+                        """)
 
 
-def delete_bookmark(bmk_name):
-    """Function returns changes that a user  enters."""
+class DeleteBookmark: # Will initiate when user selects option 'D' to delete bookmark. The book mark name the only value needed and it will be passed into the
+                      # delete_bookmark method and the sql query will be executed. 
+    def __init__(self, bmk_name):
+        self.bmk_name = bmk_name
 
-    bookmarks = c.execute(f"""DELETE FROM bookmarks WHERE bookmark_name = '{bmk_name}'""")
-    return bookmarks
+    def delete_bookmark(self): 
+        """Function returns changes that a user  enters."""
+        c.execute(f"""DELETE FROM bookmarks WHERE bookmark_name = '{self.bmk_name}'""")
 
 
 def save_changes():  # this function will be called in the client.py module to save changes made to the database.
@@ -91,3 +107,4 @@ def save_changes():  # this function will be called in the client.py module to s
 def db_conn_close():
     c.close()
     conn.close()
+    
